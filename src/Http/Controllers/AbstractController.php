182eec49892d9cd46b79567b1332f3ebd96dfa7a
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-02-20 13:04:39 +0300

    Abstract controller added

---
+++ src/Http/Controllers/AbstractController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace NextDeveloper\Support\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller;
+use NextDeveloper\Commons\Http\Traits\Responsable;
+
+class AbstractController extends Controller
+{
+    //  NextDeveloper Generator Traits
+    use Responsable;
+
+    //  Laravel Traits
+    use DispatchesJobs, ValidatesRequests;
+    // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE\n\n\n\n\n\n
+
+
+
+
+
+}
