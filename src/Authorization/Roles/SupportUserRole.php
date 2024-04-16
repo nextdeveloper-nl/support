1b34a77abbbc523bd428e490552fbd103cb44afb
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-04-16 20:38:05 +0300

    New get rules function added to authorization

--- src/Authorization/Roles/SupportUserRole.php
+++ src/Authorization/Roles/SupportUserRole.php
@@ -30,14 +30,10 @@ class SupportUserRole extends AbstractRole implements IAuthorizationRole
      */
     public function apply(Builder $builder, Model $model)
     {
-        /**
-         * Here user will be able to list all models, because by default, sales manager can see everybody.
-         */
-        $ids = AccountManagers::withoutGlobalScopes()
-            ->where('iam_account_id', UserHelper::currentAccount()->id)
-            ->pluck('crm_account_id');
-
-        $builder->whereIn('iam_account_id', $ids);
+        $builder->where([
+            'iam_account_id'    =>  UserHelper::currentAccount()->id,
+            'iam_user_id'       =>  UserHelper::me()->id
+        ]);
     }
 
     public function checkPrivileges(Users $users = null)
@@ -97,5 +93,10 @@ class SupportUserRole extends AbstractRole implements IAuthorizationRole
     public function getDbPrefix()
     {
         return self::DB_PREFIX;
+    }
+
+    public function checkRules(Users $users): bool
+    {
+        // TODO: Implement checkRules() method.
     }
 }
